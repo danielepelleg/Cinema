d9cbf7bc983925221771217455c0958e92829fb6
danielepelleg <55688016+danielepelleg@users.noreply.github.com>
2019-10-02 17:15:33 +0200

    Uploaded WCF Server
    -Deleted all conn.close() lines of code because useless when using the using().
    - String Functions Visualizza...() now return the element chosen inside a table.
    - String Functions Inserimento...() now return the element added in the db inside a table.

---
+++ WCFServer/WCFServer/IService1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFServer
+{
+    // NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di interfaccia "IService1" nel codice e nel file di configurazione contemporaneamente.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string Registration(string user_type, string username, string password, string nome, string cognome);
+
+        [OperationContract]
+        bool Login(string user_type, string username, string password);
+
+        [OperationContract]
+        string InserimentoFilm(string titolo, int anno, string regia, int durata, DateTime datauscita, string genere);
+
+        [OperationContract]
+        string CancellazioneFilm(int codicefilm);
+
+        [OperationContract]
+        string InserimentoEvento(string usernameadmin, DateTime data_e_ora, int codice_film, int codice_sala, decimal prezzo);
+
+        [OperationContract]
+        string CancellazioneEvento(int codiceevento);
+
+        [OperationContract]
+        string Visualizzazione_elenco_film();
+
+        [OperationContract]
+        string Visualizzazione_elenco_eventi();
+
+        [OperationContract]
+        string VisualizzazioneSale();
+
+        [OperationContract]
+        string ControlloFK(int valore, string value_type);
+
+        [OperationContract]
+        string Visualizzazione_elenco_UtentiFree();
+
+        [OperationContract]
+        string Visualizzazione_elenco_Prenotazioni(string user);
+
+        [OperationContract]
+        string VisualizzazionePostiDisponibili(int codice_evento);
+
+        [OperationContract]
+        string InserimentoPrenotazione(DateTime dataora, string username_utentefree, int codice_evento, int numero_posto);
+
+        [OperationContract]
+        string RappresentaSale(int codice_evento);
+
+        [OperationContract]
+        string VerificaPosto(int codice_evento, int numero_posto);
+
+    }
+}
